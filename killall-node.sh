f742a10df46bd4c9bfd77aefaaa2c9c85852bd41
Eric Lin <ttttt123465@gmail.com>
2026-04-21 17:12:57 +0800

    add script

---
+++ killall-node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title killall node
+# @raycast.mode fullOutput
+# @raycast.packageName general
+
+# Documentation:
+# @raycast.description Kills all node processes.
+killall node
